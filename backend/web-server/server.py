30327a986a78cb328cf955dff17244e8d1212b3f
AndrewCLu <andrewclu98@gmail.com>
2024-10-31 01:49:18 +0700

    hide send-encrypted-data endpoint

--- backend/web-server/server.py
+++ backend/web-server/server.py
@@ -43,49 +43,49 @@ def get_enclave_key():
     # Return the attestation document
     return jsonify(attestation_doc=attestation_doc_b64)
 
-@app.route('/send-encrypted-data', methods=['POST'])
-def send_encrypted_data():
-    try:
-        # Get encrypted data from request
-        data = request.json.get('data')
-        if not data:
-            return jsonify({}), 400
+# @app.route('/send-encrypted-data', methods=['POST'])
+# def send_encrypted_data():
+#     try:
+#         # Get encrypted data from request
+#         data = request.json.get('data')
+#         if not data:
+#             return jsonify({}), 400
 
-        # Create a vsock socket object
-        s = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
+#         # Create a vsock socket object
+#         s = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
         
-        # Fixed CID for the enclave
-        cid = 16
+#         # Fixed CID for the enclave
+#         cid = 16
 
-        # The port should match the server running in enclave
-        port = 5000
+#         # The port should match the server running in enclave
+#         port = 5000
 
-        # Connect to the server
-        s.connect((cid, port))
+#         # Connect to the server
+#         s.connect((cid, port))
 
-        # Send command to the server running in enclave
-        s.send(str.encode(json.dumps({
-            'action': 'send-encrypted-data',
-            'data': data
-        })))
+#         # Send command to the server running in enclave
+#         s.send(str.encode(json.dumps({
+#             'action': 'send-encrypted-data',
+#             'data': data
+#         })))
 
-        # Receive and decode response from the server
-        payload = s.recv(65536)
-        response = json.loads(payload.decode())
+#         # Receive and decode response from the server
+#         payload = s.recv(65536)
+#         response = json.loads(payload.decode())
 
-        # Get decrypted data from the response
-        decrypted_data = response.get('decrypted_data')
+#         # Get decrypted data from the response
+#         decrypted_data = response.get('decrypted_data')
 
-        # Close the connection 
-        s.close()
+#         # Close the connection 
+#         s.close()
 
-        # Return the decrypted data
-        return jsonify(decrypted_data=decrypted_data), 200
-    except Exception as e:
-        print(e)
+#         # Return the decrypted data
+#         return jsonify(decrypted_data=decrypted_data), 200
+#     except Exception as e:
+#         print(e)
 
-        # Return error response
-        return jsonify(error=str(e)), 500
+#         # Return error response
+#         return jsonify(error=str(e)), 500
 
 @app.route('/hash-encrypted-input', methods=['POST'])
 def hash_encrypted_input():
